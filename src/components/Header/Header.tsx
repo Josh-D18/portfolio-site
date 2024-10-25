11ce8a867da009c16be5a3990e7b75b98a7b248c
Josh <jjdate@live.com>
2024-10-25 10:40:34 -0400

    made minor changes to the header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,13 +9,13 @@ import styles from '@/components/Header/Header.module.css';
 const Header: React.FC = () => {
     return (
       <header className={styles.container}>
-      <h1 className={styles.title}>Josh Date</h1>
-      <div className={styles.imgContainer}>
-        <Image src={iconGithub} alt="github icon" width={19} height={19} className={styles.img}></Image>
-        <Image src={iconMentor} alt="frontend mentor icon" width={19} height={19} className={styles.img}></Image>
-        <Image src={iconLinkedIn} alt="linkedin icon" width={19} height={19} className={styles.img}></Image>
-        <Image src={iconTwitter} alt="twitter icon" width={19} height={19} className={styles.img}></Image>
-      </div>
+        <h1 className={styles.title}>Josh Date</h1>
+        <div className={styles.imgContainer}>
+          <Image src={iconGithub} alt="github icon" width={19} height={19} className={styles.img}></Image>
+          <Image src={iconMentor} alt="frontend mentor icon" width={19} height={19} className={styles.img}></Image>
+          <Image src={iconLinkedIn} alt="linkedin icon" width={19} height={19} className={styles.img}></Image>
+          <Image src={iconTwitter} alt="twitter icon" width={19} height={19} className={styles.img}></Image>
+        </div>
       </header>
     )
 }
